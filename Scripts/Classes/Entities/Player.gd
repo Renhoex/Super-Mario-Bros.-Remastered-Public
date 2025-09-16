a6c86d13b280febde2d3439ad278373ce9648774
LittlePlanetCD <littleplanetcd1993@gmail.com>
2025-09-16 05:10:47 -0500

    fix player phys + more accurate jump calc

--- Scripts/Classes/Entities/Player.gd
+++ Scripts/Classes/Entities/Player.gd
@@ -713,7 +713,7 @@ func jump() -> void:
 	has_jumped = true
 
 func calculate_jump_height() -> float: # Thanks wye love you xxx
-	return -(JUMP_HEIGHT + JUMP_INCR * int(abs(velocity.x) / 35))
+	return -(JUMP_HEIGHT + JUMP_INCR * int(abs(velocity.x) / 30.5))
 
 const SMOKE_PARTICLE = preload("res://Scenes/Prefabs/Particles/SmokeParticle.tscn")
 
