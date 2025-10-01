ae827a9ca1f2df44a908e8ef4aff11ffad5bc461
LittlePlanetCD <littleplanetcd1993@gmail.com>
2025-09-30 23:58:15 -0500

    why minus world

--- Scripts/Classes/LevelClass.gd
+++ Scripts/Classes/LevelClass.gd
@@ -151,7 +151,8 @@ func transition_to_next_level() -> void:
 		Global.world_num = next_world
 		LevelTransition.level_to_transition_to = get_scene_string(next_world, next_level)
 	first_load = true
-	SaveManager.write_save()
+	if Global.world_num > -1:
+		SaveManager.write_save()
 	Global.transition_to_scene("res://Scenes/Levels/LevelTransition.tscn")
 	Checkpoint.passed_checkpoints.clear()
 
